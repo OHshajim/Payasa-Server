d46bfedba2d7712c6c899071eb221eb9f886beaa
OHshajim <ajshajimmax@gmail.com>
2024-07-17 03:21:09 +0600

    file setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// server running test
+app.get('/', (req, res) => {
+    res.send('Payasa server running ...')
+})
+
+app.listen(port, () => {
+    console.log(`Payasa server listening on port ${port}`)
+})
