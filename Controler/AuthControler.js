7be24c0fc11539e23887ab98a42e4b09b052d46b
OHshajim <ajshajimmax@gmail.com>
2024-07-17 13:54:10 +0600

    mongoose connected

--- Controler/AuthControler.js
+++ Controler/AuthControler.js
@@ -1,4 +1,5 @@
 const UserModel = require("../Models/User");
+const jwt = require('jsonwebtoken')
 const bcrypt = require("bcrypt");
 
 const registration = async (req, res) => {
@@ -22,4 +23,30 @@ const registration = async (req, res) => {
 };
 
 
-module.exports = { registration}
+const Login = async (req, res) => {
+  try {
+    const { number, email, pin } = req.body;
+    const user = await UserModel.findOne({ email, number });
+    if (!user) {
+      return res
+        .status(403)
+        .json({ message: "User undefine ,Please register now", success: false });
+    }
+    
+    const pinValidate = await bcrypt.compare(pin, user.pin);
+    if (!pinValidate) {
+        return res
+          .status(403)
+          .json({ message: "Please remember your account pin !!!", success: false });
+      }
+    await NewUser.save();
+    res
+      .status(201)
+      .json({ message: "User successfully created", success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server error", success: false });
+  }
+};
+
+
+module.exports = { registration ,Login}
