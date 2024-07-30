85a8959f6c9b69ef66b3f074926403469d1cd9a2
OHshajim <ajshajimmax@gmail.com>
2024-07-30 16:00:16 +0600

    send number validation

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config();
-const bodyParser = require('body-parser')
-require('./Models/db')
-const Authentication = require('./Router/Authentication')
+require("dotenv").config();
+const bodyParser = require("body-parser");
+require("./Models/db");
+const Authentication = require("./Router/Authentication");
+const UserModel = require("./Models/User");
 const app = express();
 const port = process.env.PORT || 5000;
-
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json())
-app.use('/authentication',Authentication)
+app.use(bodyParser.json());
+app.use("/authentication", Authentication);
 
+app.get("/numberValidate/:number", async (req, res) => {
+  const { number } = req.params;
+  console.log(number);
+  const result =await UserModel.findOne({ number: number });
+  result? res.send({message:"User A/C is Valid",success:true,number:result.number}):res.send({success:false , message: "This user A/C is not valid !!!"})
+});
 
-// server running test 
-app.get('/', (req, res) => {
-    res.send('Payasa server running ...')
-})
+// server running test
+app.get("/", (req, res) => {
+  res.send("Payasa server running ...");
+});
 
 app.listen(port, () => {
-    console.log(`Payasa server listening on port ${port}`)
-})
+  console.log(`Payasa server listening on port ${port}`);
+});
